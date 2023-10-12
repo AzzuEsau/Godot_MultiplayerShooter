de75caa10c38888756332129b18986b8db72a523
AzzuEsau <andres.lopez4191@alumnos.udg.mx>
2023-10-12 14:53:15 -0600

    start project

---
+++ scritps/scenes/MainMenu.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Node2D {
+	#region Variables
+		// [Export] private int vairbaleInEditor;
+	#endregion
+
+	#region Signals
+		// [Signal] public delegate void ExampleSignalEventHandler();
+	#endregion
+
+	#region Godot Methdos
+		public override void _Ready() {
+
+		}
+
+		public override void _Process(double delta) {
+
+		}
+	#endregion
+
+	#region My Methods
+	#endregion
+
+	#region Events
+	#endregion
+}
